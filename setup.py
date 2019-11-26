9756f83828970895e8a57e2acd5c5488daa67a22
manuel_vasquez <manuelvsqz@gmail.com>
2019-11-26 11:03:46 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.0.1",
     author="José Manuel Vásquez",
     author_email="manuelvsqz@gmail.com",
-    description="An unified command line tool to submit jobs to Torque, LSF or SLURM clusters",
+    description="An unified command line tool to submit jobs to PBS, LSF or SLURM queues",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
     url="https://github.com/cronofugo/python-j2q",
