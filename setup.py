e14228d7f13652b5d5bc9e9e0891852023e74423
cronofugo <jmvasquez@helio.helio>
2021-02-22 13:50:45 -0600

    Clean setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-# Minimum Setuptools version supporting configuration metadata in setup.cfg
-#min_stversion = '30.3'
-# Minimum Setuptools version supporting conditional python dependencies (PEP 508)
-min_stversion = '32.2'
-
-import sys
 import setuptools
-from time import time
 
-# Record time before setup
-setup_time = time()
+#min_stversion = '30.3' # Minimum Setuptools version supporting configuration metadata in setup.cfg
+min_stversion = '32.2' # Minimum Setuptools version supporting conditional python dependencies (PEP 508)
 
 # Setup package if Setuptools version is high enough
 setuptools.setup(setup_requires=['setuptools>=' + min_stversion])
